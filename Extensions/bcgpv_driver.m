4bd8d8b7c8b73d61416d902f8a08e1badb3a6fe4
QuRyu <Quentin.Liu.0415@gmail.com>
2021-01-09 14:33:05 +0800

    update doc

--- Extensions/bcgpv_driver.m
+++ Extensions/bcgpv_driver.m
@@ -1,11 +1,9 @@
-% � Brent R. Hickman and Timothy P. Hubbard
-% hickmanbr@uchicago.edu and timothy.hubbard@colby.edu
+% Code for "Validate Structural Analysis"
 
-% please cite our Journal of Applied Econometrics paper "Replacing Sample
+% Reference:
+% "Brent R. Hickman and Timothy P. Hubbard (2015). Replacing Sample
 % Trimming with Boundary Correction in Nonparametric Estimation of
-% First-Price Auctions" if this code is helpful to you.
-
-% a sample driver file using uniformly distributed data
+% First-Price Auctions. Journal of Applied Econometrics"
 
 clear all
 clc
@@ -14,17 +12,6 @@ clc
 lwidth = 2;
 fsize = 14;
 set(0,'defaulttextinterpreter','latex','Defaulttextfontsize',fsize);
-
-% load your data here as a column vector!
-% b = data
-% N = from your data
-% sample uniform data from [0,1/2] with N = 2
-% b = rand(600,1);
-% b = b/2;
-% N = 2;
-% bmax = max(b);
-% bmin = min(b);
-% b = sort(b);
 
 % use hardle bandwidth transformation constant for non-gaussian kernel
 usehardle = 1;
@@ -47,25 +34,8 @@ denom_t1 = 0 - (-2 + 18/4 - 12/5);
 denom_t2 = quad(@evalkspdf_denom,-1,1,1e-14,[],kernel);
 b0 = ((num_t1^2*num_t2)/(denom_t1^2*denom_t2))^(1/5);
 
-
-% [gB_bc,hb_l] = kspdf_bc(b,kernel,b,b0,usehardle);
-% % right-boundary correction: reflect bids over zero and do left-boundary
-% % correction 
-% [gB_bc_right,hb_r] = kspdf_bc(-b,kernel,-b,b0,usehardle);
-% bind = find(b >= bmax - hb_r);
-% gB_bc(bind) = gB_bc_right(bind);
-% GB = kscdf(b,'edf',b);
-% v_bc = b' + GB./(gB_bc*(N - 1));
-% v_bc = v_bc';
-% vmin_bc = min(v_bc);
-% vmax_bc = max(v_bc);
-
-data = readtable('data/DataProcessed.csv');
+data = readtable('../data/DataProcessed.csv');
 data.PercentOfEstimates = data.PercentOfEstimates .* 100;
-% data(find(data.PercentOfEstimates < 20), :) = []; 
-% data(find(data.PercentOfEstimates > 200), :) = []; 
-
-
 
 % parameters 
 v = [];
@@ -108,8 +78,6 @@ v_bc = v;
 inf_idx = find(isinf(v_bc));
 v_bc(inf_idx) = []; 
 bids(inf_idx) = []; 
-% gB_bc_global(inf_idx) = [];
-
 
 vmax_bc = max(v_bc);  
 
@@ -117,14 +85,6 @@ vmax_bc = max(v_bc);
 % optional valuation points to evaluate each estimator at
 neval = 1000;
 evalpts = linspace(min(v_bc),max(v_bc),neval);
-
-% figure 
-% set(gcf,'DefaultLineLineWidth',lwidth)
-% set(gca,'FontSize',fsize)
-% scatter(v_bc, data.PercentOfEstimates, '.b')
-% xlabel('$\mathbf{\hat{c}}$')
-% ylabel('$\mathbf{b}$')
-% box on
 
 % step 2: recover valuation densities using boundary correction
 [fV_bc,hv_l] = kspdf_bc(v_bc,kernel,evalpts,b0,usehardle);
@@ -160,9 +120,9 @@ if plotind == 1
     set(gcf,'DefaultLineLineWidth',lwidth)
     set(gca,'FontSize',fsize)
     plot(evalpts,fV_bc)
-	xlabel('$\mathbf{\hat{c}}$')
-	ylabel('$\mathbf{\hat{f}_C(c)}$')
-	box on
+    xlabel('$\mathbf{\hat{c}}$')
+    ylabel('$\mathbf{\hat{f}_C(c)}$')
+    box on
     
     % valuation distribution
     figure 
@@ -179,12 +139,13 @@ evalpts = linspace(min(v_bc), max(v_bc), max(v_bc)-min(v_bc));
 FV = kscdf(v_bc, 'edf', evalpts);
 
 if simulind == 1
-    b = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    N = 2;
+    N = 16;
+    b = zeros(N, 1);
+    c = zeros(N, 1);
+    run = 1000;
     
-    
-    
-    for i = 1:1000
+    % not vectorized here because we may need to draw a different random number 
+    for i = 1:run
         for j = 1:10 
             
             while true 
@@ -200,7 +161,7 @@ if simulind == 1
                     end 
                 
                     b(j) = b(j) + cost + ... 
-                        trapz(evalpts(closestIdx:end), 1-FV(closestIdx:end)) / (1-FV(closestIdx))^N;
+                        trapz(evalpts(closestIdx:end), (1-FV(closestIdx:end)).^(N-1)) / (1-FV(closestIdx))^(N-1);
                 else 
                     cost = mean(evalpts(idx));
                     
@@ -209,13 +170,23 @@ if simulind == 1
                     end 
                     
                     b(j) = b(j) + cost + ... 
-                        trapz(evalpts(idx(1):end), 1-FV(idx(1):end)) / (1-prob(i, j))^N;
+                        trapz(evalpts(idx(1):end), (1-FV(idx(1):end)).^(N-1)) / (1-prob(i, j))^(N-1);
                 end 
+                c(j) = c(j) + cost; 
                 
                 break
             end 
         end 
     end 
     
-    b = b ./ 1000;
+    b = b ./ run;
+    c = c ./ run;
+    
+    figure
+    set(gcf,'DefaultLineLineWidth',lwidth)
+    set(gca,'FontSize',fsize)
+    scatter(c, b,'.b')
+    xlabel('$\mathbf{\hat{c}}$')
+    ylabel('$\mathbf{b}$')
+    box on
 end 
